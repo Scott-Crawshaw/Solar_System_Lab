646b9bea1929c178ac34cc4aae938c9b4e72cd29
Scott Crawshaw <scott.crawshaw@gmail.com>
2019-02-11 17:19:42 -0500

    working on calculating acceleration

--- body.py
+++ body.py
@@ -1,9 +1,26 @@
+from cs1lib import *
+
 class Body:
-    def __init__(self):
-        pass
+    def __init__(self, mass, x, y, vx, vy, pixel_radius, r, g, b):
+        self.mass = mass
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.radius = pixel_radius
+        self.red = r
+        self.green = g
+        self.blue = b
 
     def draw(self):
-        pass
+        disable_stroke()
+        set_fill_color(self.red, self.green, self.blue)
+        draw_circle(self.x, self.y, self.radius)
 
-    def update(self):
-        pass
+    def update_velocity(self, ax, ay, timestep):
+        self.vx += (ax * timestep)
+        self.vy += (ay * timestep)
+
+    def update_position(self, timestep):
+        self.x += (self.vx * timestep)
+        self.y += (self.vy * timestep)
--- system.py
+++ system.py
@@ -1,6 +1,50 @@
+from math import sqrt
+
 class System:
-    def __init__(self):
-        pass
+    GRAVITY = 6.67384E-11
+
+    def __init__(self, body_list, timestep=1):
+        self.bodies = body_list
+        self.timestep = timestep
 
     def draw(self):
-        pass
+        for body in self.bodies:
+            body.draw()
+
+    def update(self):
+        for body in self.bodies:
+            ax, ay = self.calculate_acceleration(body)
+            body.update_velocity(ax, ay, self.timestep)
+
+    def calculate_acceleration(self, obj):
+        ax = 0
+        ay = 0
+
+        for body in self.bodies:
+            if body.x != obj.x or body.y != obj.y:
+                numerator = System.GRAVITY * body.mass
+                distance = self.get_r(obj, body)
+                denominator = distance ** 2
+                acceleration = numerator / denominator
+                partial_ax, partial_ay = self.get_accel_components(acceleration, distance, obj, body)
+                ax += partial_ax
+                ay += partial_ay
+
+        return ax, ay
+
+    def get_r(self, body1, body2):
+        dy = body2.y - body1.y
+        dx = body2.x - body1.x
+
+        distance = sqrt(dx ** 2 + dy ** 2)
+
+        return distance
+
+    def get_accel_components(self, acceleration, distance, body1, body2):
+        dy = body2.y - body1.y
+        dx = body2.x - body1.x
+
+        ax = acceleration * (dx / distance)
+        ay = acceleration * (dy / distance)
+
+        return ax, ay
